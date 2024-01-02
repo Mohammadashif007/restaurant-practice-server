7a47c6d3571b6fb9693d6dc043b706d74d69a328
Mohammad Ashif <Mohammad Ashif>
2024-01-02 13:35:12 +0600

    delete api added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // mongodb connection
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.u69fsfj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -63,8 +63,10 @@ async function run() {
     // })
 
     app.get('/carts', async(req, res) => {
+      // const email = req.query.email;
+      // const query = {email: email}
       const email = req.query.email;
-      const query = {email: email}
+      const query = {email : email}
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     })
@@ -80,6 +82,14 @@ async function run() {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
+    })
+
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
     })
 
 
